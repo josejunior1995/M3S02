753d45941d84b04909db55cdcd27a9a5f17dc151
José Junior <jose_lopes-junior@estudante.sesisenai.org.br>
2023-06-15 10:33:16 -0300

    Exercicio03

---
+++ m3s02EX03/src/main/java/br/com/canasvieiras/m3s02projetorevisao/services/DespesaService.java
@@ -0,0 +1,27 @@
+package br.com.canasvieiras.m3s02projetorevisao.services;
+
+import br.com.canasvieiras.m3s02projetorevisao.entities.Despesa;
+import br.com.canasvieiras.m3s02projetorevisao.repositories.DespesaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DespesaService {
+
+    @Autowired
+    private DespesaRepository despesaRepository;
+
+    public Despesa lançarDespesa(Despesa despesa) {
+        despesa.setStatus("Pendente");
+        despesa.setDataPagamento(null);
+        return despesaRepository.save(despesa);
+    }
+
+    public Despesa buscarPorId(Long id) {
+        return null;
+    }
+
+    public Despesa salvar(Despesa despesaExistente) {
+        return null;
+    }
+}
